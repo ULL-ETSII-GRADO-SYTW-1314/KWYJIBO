2f3ce7022f0d7c0f56934942ca277d18c510bdd1
pasku14 <pasku14@gmail.com>
2013-12-06 21:11:33 +0000

    Arreglando mostrar rectas con foto

--- Kwyjibo/settings.py
+++ Kwyjibo/settings.py
@@ -64,7 +64,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(SITE_ROOT, 'media/')
+MEDIA_ROOT = os.path.join(SITE_ROOT, '../static/images/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -84,6 +84,7 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, '../static'),
+    os.path.join(SITE_ROOT, '../static/images/media'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
