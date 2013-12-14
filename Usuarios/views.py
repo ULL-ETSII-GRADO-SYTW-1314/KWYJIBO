56d3d7869d222f36562ad0f11109ea649d26f3eb
pasku14 <pasku14@gmail.com>
2013-12-14 12:21:04 +0000

    Arreglando Login

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -90,6 +90,7 @@ def Registro_Usuario(request):
 		return render_to_response('usuarios/registrate.html', {'form':form}, context_instance=RequestContext(request))
 
 def LogIn(request):
+
 	if request.method == 'POST':
 		form = Form_Auth_Usuario(request.POST)
 		errores =[]
@@ -97,29 +98,23 @@ def LogIn(request):
 		if form.is_valid:
 			nick = request.POST['nick']
 			password = request.POST['password']
-			listado = Usuario.objects.get
 
 			try:
-				a = listado(nick=nick)
-			except: 
-				errores.append('El usuario no existe')
-				return render_to_response('usuarios/login.html', {'form':form,'errores':errores}, context_instance=RequestContext(request))
-
-			else:
+				a = Usuario.objects.get(nick=nick)
 				if a.password == request.POST['password']:
 					request.session['nick'] = a.nick
 					request.session['id'] = a.id
 					a.save()
-
-					return HttpResponseRedirect('/')
 					print 'Todo OK'
-
-				else:
-					errores.append('El usuario no coincide')
-					return render_to_response('usuarios/login.html', {'form':form,'errores':errores}, context_instance=RequestContext(request))
+					return render_to_response('kwyjibo/index.html', context_instance=RequestContext(request))
+			except:
+				errores.append('El usuario no existe o contrasena incorrecta')
+				return render_to_response('usuarios/login.html', {'form':form,'errores':errores}, context_instance=RequestContext(request))
+			else:
+				errores.append('El usuario no coincide o contrasena incorrecta')
+				return render_to_response('usuarios/login.html', {'form':form,'errores':errores}, context_instance=RequestContext(request))
 	else:
 		form = Form_Auth_Usuario()
-		print "pepe";
 		return render_to_response('usuarios/login.html', {'form':form}, context_instance=RequestContext(request))
 
 def LogOut(request):
@@ -152,7 +147,7 @@ def Session(request):
 			print "Session.alguien"
 			print request.session['nick']
 			sesion = True
-		return render_to_response('kwyjibo/index.html', {'sesion':sesion}, context_instance=RequestContext(request))
+		return render_to_response('base.html', {'sesion':sesion}, context_instance=RequestContext(request))
 	except:
 		print sesion
-		return render_to_response('kwyjibo/index.html', {'sesion':sesion}, context_instance=RequestContext(request))
+		return render_to_response('base.html', {'sesion':sesion}, context_instance=RequestContext(request))
